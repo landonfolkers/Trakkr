06b47a5c14efd90521b04adc0998efd465be92a0
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-05-07 15:32:50 -0600

    added big buttons

---
+++ trakkr/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert, AppRegistry } from 'react-native';
+import Buttons from './components/Buttons'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.title}>
+      <Text>Trakkr</Text>
+      <Buttons />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
---
+++ trakkr/components/Buttons.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert, AppRegistry, TouchableHighlight, Dimensions} from 'react-native';
+
+export default class Buttons extends React.Component {
+  onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight style={styles.button} onPress={this.onPressButton} underlayColor="white">
+          <View>
+            <Text style={styles.buttonText}>Level 1</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button2} onPress={this.onPressButton}underlayColor="white">
+        <View>
+          <Text style={styles.buttonText}>Level 2</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight style={styles.button3} onPress={this.onPressButton} underlayColor="white">
+      <View>
+        <Text style={styles.buttonText}>Level 3</Text>
+      </View>
+    </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+var width = Dimensions.get('window').width
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  button: {
+    flex: 2,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    width: width,
+  },
+  button2: {
+    flex: 2,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button3: {
+    flex: 2,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 30,
+    color: 'white'
+  }
+})
